1a08903df77a5367f6a430973a57c468a9fcdac4
unknown <jdsuplito@gmail.com>
2023-06-30 16:09:53 +0800

    Add changes

---
+++ .history/script_20230630153222.js
@@ -0,0 +1,33 @@
+// This targets the class of the side navigation buttons remains active when clicked, and removes it when not.
+const anchorButtons = document.querySelectorAll(".js-active-btn");
+anchorButtons.forEach((btn) => {
+  btn.addEventListener("click", makeButtonActive);
+});
+
+const viewerBtn = document.querySelectorAll(".css-img-btn");
+function makeButtonActive(event) {
+  anchorButtons.forEach((btn) => {
+    btn.classList.remove("active");
+  });
+
+  event.target.classList.add("active");
+}
+
+for (let i = 0; i < viewerBtn.length; i++) {
+  viewerBtn[i].addEventListener("click", function (e) {
+    let imgSrc = e.target.getAttribute("data-img-src");
+    console.log(e.target);
+    console.log("hello", imgSrc);
+    document.getElementById("img-modal").src = imgSrc;
+  });
+}
+
+// bootstrap tooltips trigger
+const tooltipTriggerList = document.querySelectorAll(
+  '[data-bs-toggle="tooltip"]'
+);
+const tooltipList = [...tooltipTriggerList].map(
+  (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
+);
+
+AOS.init();
